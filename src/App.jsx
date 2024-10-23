1a85d24d54bf0331a6547e558aac67e304884bf8
sakibMERN <sakib.sr57@gmail.com>
2024-10-23 15:23:30 +0600

    add fake json data and make Blogs component and fetch data into it

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 
 import './App.css'
+import Blogs from './components/Blogs/Blogs'
+import Header from './components/Header/Header'
 
 function App() {
   
 
   return (
     <>
-      <h1 className='text-5xl font-black'>I love Bangladesh</h1>
+      <Header></Header>
+      <Blogs></Blogs>
     </>
   )
 }
