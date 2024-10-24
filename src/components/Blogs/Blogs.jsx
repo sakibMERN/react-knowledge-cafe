df84702ad3f4fa0c0b13b9d16d350e01ae214348
sakibMERN <sakib.sr57@gmail.com>
2024-10-24 17:55:16 +0600

    fix body width and add react icon

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -3,22 +3,25 @@ import { useEffect } from "react";
 import { useState } from "react";
 import Blog from "../Blog/Blog";
 
-
 const Blogs = () => {
-    const [blogs, setBlogs] = useState([]);
+  const [blogs, setBlogs] = useState([]);
 
-    useEffect(()=> {
-        fetch('blogs.json')
-        .then(res => res.json())
-        .then(data => setBlogs(data))
-    },[])
-    console.log(blogs);
-    return (
-        <div className="md:w-2/3">
-            <h2>Blogs: {blogs.length}</h2>
-            <Blog></Blog>
-        </div>
-    );
+  useEffect(() => {
+    fetch("blogs.json")
+      .then((res) => res.json())
+      .then((data) => setBlogs(data));
+  }, []);
+  // console.log(blogs);
+  return (
+    <div className="md:w-2/3">
+      <h2>Blogs: {blogs.length}</h2>
+      {blogs.map((blog) => (
+        <Blog key={blog.id} 
+        blog={blog}
+        ></Blog>
+      ))}
+    </div>
+  );
 };
 
 export default Blogs;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,7 @@ import profile from "../../assets/images/profile.png"
 
 const Header = () => {
     return (
-        <header className="flex justify-between items-center p-8 border-b-2">
+        <header className="flex justify-between items-center py-8 border-b-2">
             <h1 className="text-4xl font-bold">Knowledge Cafe</h1>
             <img src={profile} alt="" />
         </header>
